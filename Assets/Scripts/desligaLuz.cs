5e53d4b37a1ecf59b4f0c1898abc9272dcd14383
BrunoMMachadoFreitas <senhorragnaros@gmail.com>
2021-04-08 23:46:41 +0100

    Cobri o cenario e criei o script de ligar/desligar a luz

---
+++ Assets/Scripts/desligaLuz.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desligaLuz : MonoBehaviour
+{
+    public Light luzParaDesligar;
+    
+    public void desligarLuz()
+    {
+        if(luzParaDesligar.intensity > 0)
+        {
+            luzParaDesligar.intensity = 0;
+        }
+        else if(luzParaDesligar.intensity == 0)
+        {
+            luzParaDesligar.intensity = 7;
+        }
+    }
+}
